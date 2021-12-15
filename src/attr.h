501afaa008bbbe15ad95dc7d110d03034f8f04e0
garou <xgaroux@gmail.com>
2021-12-16 01:13:19 +0300

    Added attr.h for platform-specific attributes definitions

---
+++ src/attr.h
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * @file    attr.h
+ * @author  garou (xgaroux@gmail.com)
+ * @brief   Header file of some cross-platform attributes definition.
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __ATTR_H
+#define __ATTR_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#if defined(ESP_PLATFORM)
+/* ESP Section ---------------------------------------------------------------*/
+
+#include "esp_attr.h"
+
+#define NOINIT_ATTR                 __NOINIT_ATTR
+#define RETAIN_NOINIT_ATTR          RTC_NOINIT_ATTR
+
+#else
+/* Unknown platform ----------------------------------------------------------*/
+
+#define NOINIT_ATTR
+#define RETAIN_NOINIT_ATTR
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ATTR_H */
+
+/***************************** END OF FILE ************************************/
