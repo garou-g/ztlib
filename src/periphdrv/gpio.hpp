68e6d4aa11cafe3ce10603f1f0b8253e9f0fbac6
garou <xgaroux@gmail.com>
2022-09-08 11:13:23 +0300

    Added new module - gpio - for simple hw independent gpio control

---
+++ src/periphdrv/gpio.hpp
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * @file    gpio.hpp
+ * @author  garou (xgaroux@gmail.com)
+ * @brief   Header file of abstract gpio driver.
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __GPIO_DRIVER_H
+#define __GPIO_DRIVER_H
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**
+ * @brief Abstract gpio driver
+ */
+class Gpio {
+public:
+    struct Config {
+        int32_t pin;
+    };
+
+    enum IoctlCmd {
+    };
+
+    /**
+     * @brief Default constructor. Almost empty
+     */
+    Gpio();
+
+    /**
+     * @brief Open driver with selected config
+     *
+     * @param drvConfig driver configuration
+     * @return true if success opened otherwise false
+     */
+    virtual bool open(const void* const drvConfig) = 0;
+
+    /**
+     * @brief Check driver opened state
+     *
+     * @return true if opened otherwise false
+     */
+    bool isOpen() const;
+
+    /**
+     * @brief Close driver
+     */
+    virtual void close() = 0;
+
+    /**
+     * @brief Sets configured gpio to high level
+     */
+    virtual int32_t set() = 0;
+
+    /**
+     * @brief Sets configured gpio to low level
+     */
+    virtual int32_t reset() = 0;
+
+    /**
+     * @brief Execute chosen command on driver
+     *
+     * @param cmd command to execute
+     * @param pValue pointer for command data
+     * @return true if command executed successfully otherwise false
+     */
+    virtual bool ioctl(uint32_t cmd, void* const pValue) = 0;
+
+protected:
+    /**
+     * @brief Set opened state of driver from successor classes
+     *
+     * @param state new opened state
+     */
+    void setOpened(bool state);
+
+private:
+    bool opened;
+};
+
+#endif /* __GPIO_DRIVER_H */
+
+/***************************** END OF FILE ************************************/
---
+++ src/periphdrv/hw/espgpio.hpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * @file    espgpio.hpp
+ * @author  garou (xgaroux@gmail.com)
+ * @brief   Header file of ESP32 GPIO driver.
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __ESP_GPIO_DRIVER_H
+#define __ESP_GPIO_DRIVER_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "driver/gpio.h"
+
+#include "gpio.hpp"
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+class EspGpio final : public Gpio {
+public:
+    EspGpio();
+
+    bool open(const void* const drvConfig) override;
+    void close() override;
+
+    int32_t set() override;
+    int32_t reset() override;
+
+    bool ioctl(uint32_t cmd, void* const pValue) override;
+
+private:
+    gpio_num_t pin;
+};
+
+#endif /* __ESP_GPIO_DRIVER_H */
+
+/***************************** END OF FILE ************************************/
