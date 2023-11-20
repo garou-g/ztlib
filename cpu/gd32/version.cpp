a738196648a35dd4ab4546dae6005d5240a085f7
garou <xgaroux@gmail.com>
2023-11-20 16:56:21 +0300

    Added using of version definition if exists in gd32

---
+++ cpu/gd32/version.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * @file    version.cpp
+ * @author  garou (xgaroux@gmail.com)
+ * @brief   GD32 version driver module.
+ ******************************************************************************/
+
+#include "version.h"
+
+/**
+ * @brief Read HW version voltages and return it
+ *
+ * @return Version::HwVolt structure with HW voltages
+ */
+Version::HwVolt Version::getHwValue()
+{
+    HwVolt hw;
+    hw.major = 0;
+    hw.minor = 0;
+    return hw;
+}
+
+/**
+ * @brief Read FW version data and return it
+ *
+ * @return Version::FwString structure with FW version
+ */
+Version::FwString Version::getFwValue()
+{
+    FwString fw = {
+#if defined(VERSION_NUMBER)
+        .data = VERSION_NUMBER
+#else
+        .data = "0.0.0.0"
+#endif
+    };
+    return fw;
+}
+
+/***************************** END OF FILE ************************************/
