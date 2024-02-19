cd33a9c626d383ab3972c96c919f317526833aa0
garou <xgaroux@gmail.com>
2024-02-19 17:09:11 +0300

    Added interface for timer driver classes
    Signed-off-by: doctorbelka <avdbor.junior@mail.ru>

---
+++ cpu/gd32/timer.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * @file    timer.h
+ * @author  doctorbelka avdbor.junior@mail.ru
+ * @brief   Header file of timer.
+ ******************************************************************************/
+
+#pragma once
+
+#include "periph/timer.h"
+#include "gd32/gd32_types.h"
+
+namespace gd32 {
+
+/// @brief Timer configuration data structure
+struct TimerConfig {
+    int32_t timer;
+    uint16_t prescaler;
+    uint32_t period;
+    TimerMode mode;
+    GpioConfig pin;
+};
+
+/// @brief GD32 Timer peripheral driver
+class Timer : public ::Timer {
+public:
+    Timer() = default;
+
+    bool setConfig(const void* drvConfig) override;
+    bool open() override;
+    void close() override;
+
+    bool ioctl(uint32_t cmd, void* pValue) override;
+
+    void start() override;
+    void stop() override;
+
+    uint32_t captured() override;
+
+private:
+    uint32_t captured_ = 0;
+    TimerConfig config_;
+};
+
+}; // namespace gd32
+
+/***************************** END OF FILE ************************************/
---
+++ include/periph/timer.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * @file    timer.h
+ * @author  doctorbelka avdbor.junior@mail.ru
+ * @brief   Header file of timer.
+ ******************************************************************************/
+
+#pragma once
+
+#include "basedrv.h"
+
+/// @brief Timer mode
+enum TimerMode {
+    General,
+    Capture,
+    Pwm
+};
+
+/// @brief Timer peripheral driver
+class Timer : public BaseDrv {
+public:
+    enum IoctlCmd {
+        kSetPrescale,   // Sets timer prescale
+        kSetPeriod,     // Sets repetition value
+    };
+
+    /// @brief Start timer
+    virtual void start() = 0;
+    /// @brief Stop timer
+    virtual void stop() = 0;
+
+    virtual uint32_t captured() = 0;
+};
+
+/***************************** END OF FILE ************************************/
